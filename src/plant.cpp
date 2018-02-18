4c2d8a5e36fb1c7c338664cb4562bbe37e091bcc
BrenEng <br@disroot.org>
2018-02-18 10:58:07 +0100

    Project ideas

---
+++ src/plant.cpp
@@ -0,0 +1,13 @@
+  /**
+Script keywords:
+  *Temperature
+  *Soil humidity
+  *Air humidity
+
+  *Shown on an alternating LCD display'
+
+Possible additions:
+  *Water level (as soil humidity)
+  *Data logging local or online (Wemos)
+
+*/
