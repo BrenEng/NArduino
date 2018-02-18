04df3dd8eb71932812be6baa0fe1486b4eb36e48
BrenEng <br@disroot.org>
2018-02-18 17:42:42 +0100

    Progress, temp, soil and LCD.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,70 @@
-   /**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified module
+//I2C device found at address 0x3F
+// Data wire is plugged into pin 2 on the Arduino
 
-void setup()
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+int sensor_pin = A0;
+int output_value ;
+
+void setup(void)
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  lcd.setBacklightPin(3,POSITIVE);
+  lcd.setBacklight(HIGH); // NOTE: You can turn the backlight off by setting it to LOW instead of HIGH
+  lcd.begin(16, 2);
+  lcd.clear();
+  Wire.begin();
+  // start serial port
+  Serial.begin(9600);
+  //Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
 }
 
-void loop()
+
+void loop(void)
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  //Serial.print(" Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  //Serial.println("DONE");
 
-  // wait for a second
-  delay(1000);
+  Serial.print("Temperature: ");
+  Serial.println(sensors.getTempCByIndex(0)); // Why "byIndex"?
+    // You can have more than one IC on the same bus.
+    // 0 refers to the first IC on the wire
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+  output_value= analogRead(sensor_pin);
+  output_value = map(output_value,1023,420,0,100);
+  Serial.print("Mositure: ");
+  Serial.print(output_value);
+  Serial.println(" %");
+  lcd.setBacklight(LOW);      // Backlight off
+  delay(3000);
+  lcd.setBacklight(HIGH);     // Backlight on
+  delay(3000);
 
-   // wait for a second
-  delay(1000);
+  lcd.setCursor(0,0);
+  lcd.print("http://mklec.com");
+  lcd.setCursor(0,1);
+  lcd.print("I2C Module Demo");
+
+
+  delay(5000);           // wait 5 seconds for next scan
+
 }
